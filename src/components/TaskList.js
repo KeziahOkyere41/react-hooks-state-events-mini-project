48c4449cefec392d6b6007573404fae2fe76518e
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-08-04 09:12:43 +0000

    completed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Task from "./Task";
+import {nanoid} from "nanoid";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
-}
-
+function TaskList({tasks}) {  
+  console.log(tasks)
+      return (    
+      <div className="tasks">      
+      {tasks.map(task => <Task key={nanoid(5)} text = {task.text} category={task.category}/>)  }    
+      </div>  
+      );
+    }
 export default TaskList;
